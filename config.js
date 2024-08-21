bda1fce12c2a20b004672e7827d375d8114cf064
V3lheiz <77874462+V3lheiz@users.noreply.github.com>
2024-08-21 07:47:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+global.owner = ['6282131094894']  
+global.mods = ['6282131094894'] 
+global.prems = ['6282131094894']
+global.nameowner = 'V3lheiz'
+global.numberowner = '6282131094894' 
+global.mail = 'zieroblank07@gmail.com' 
+global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
+global.instagram = '-'
+global.wm = '© Tio'
+global.wait = '_*Tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.packname = 'Made With'
+global.author = 'Bot WhatsApp'
+global.autobio = true // Set true untuk mengaktifkan autobio
+global.maxwarn = '2' // Peringatan maksimum
+
+//INI WAJIB DI ISI!//
+global.btc = 'velhei' 
+//Daftar terlebih dahulu https://api.botcahx.eu.org
+
+//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
+global.lann = 'YOUR_APIKEY_HERE'
+//Daftar https://api.betabotz.eu.org 
+
+global.APIs = {   
+  btc: 'https://api.botcahx.eu.org'
+}
+global.APIKeys = { 
+  'https://api.botcahx.eu.org': 'velhei' 
+}
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
